f3bc8e807205df7e021c6a64ad5cb61275a312a6
8molik <blejzoo@gmail.com>
2023-06-16 12:55:51 +0200

    Added collisions

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ HEIGHT = 600
 WIDTH = 400
 GAP = 150
 
+#FPS = 60
 PIPE_WIDTH = 65
 PIPE_HEIGHT = 400
 BIRD_WIDTH = 42
 BIRD_HEIGHT = 29
+
+PIPE_HIT = pygame.USEREVENT + 1
 
 # Klasa abstrakcyjna reprezentująca obiekt w grze
 class GameObject(ABC):
@@ -27,12 +30,15 @@ class GameObject(ABC):
     def display(self, WIN):
         pass
 
+    @abstractmethod
+    def get_rect(self):
+        pass
+
 
 # Klasa reprezentująca tło
 class Background(GameObject):
 
     def __init__(self, x = 0, y = 0, back_width = WIDTH, back_height = HEIGHT):
-
         super().__init__(x, y, back_width, back_height)
         self.back_image = pygame.transform.scale(pygame.image.load(
             os.path.join('assets/GameObjects', 'background.png')), (self.width, self.height))
@@ -40,16 +46,22 @@ class Background(GameObject):
     def display(self, WIN):
         WIN.blit(self.back_image, (self.x, self.y))
 
+    def get_rect(self):
+        pass
+
 class Base(GameObject):
 
     def __init__(self, x, y, base_width = WIDTH, base_height = 100):
         super().__init__(x, y , base_width, base_height)
         self.base_image = pygame.transform.scale(pygame.image.load(
             os.path.join('assets/GameObjects', 'base.png')), (self.width, self.height))
-
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         
     def display(self, WIN):
         WIN.blit(self.base_image, (self.x, self.y))
+
+    def get_rect(self):
+        return self.rect
 
 # Klasa reprezentująca ptaka
 class Bird(GameObject):
@@ -59,19 +71,23 @@ class Bird(GameObject):
 
         self.bird_image = pygame.transform.scale(
             pygame.image.load(os.path.join('assets/GameObjects', 'bird-midflip.png')), (self.width, self.height))
-        self.rect = self.bird_image.get_rect()
-        self.vel = -3
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.vel = 0
     
     def jump(self):
         if self.y > 0:
-            self.vel = -3
+            self.vel = -2.5
     
     def update(self):
         self.y += self.vel
         self.vel += 0.05
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
     
     def display(self, WIN):
         WIN.blit(self.bird_image, (self.x, self.y))
+
+    def get_rect(self):
+        return self.rect
 
 class Pipe(GameObject):
     def __init__(self, x, y, rotate = False, pipe_width = PIPE_WIDTH, pipe_height = PIPE_HEIGHT):
@@ -79,7 +95,7 @@ class Pipe(GameObject):
         self.rotate = rotate
         self.pipe_image = pygame.transform.scale(
             pygame.image.load(os.path.join('assets/GameObjects', 'pipe.png')), (self.width, self.height))
-        self.rect = self.pipe_image.get_rect()
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def display(self, WIN):
         if self.rotate:
@@ -89,6 +105,11 @@ class Pipe(GameObject):
 
     def move(self):
         self.x -= 0.5
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def get_rect(self):
+        return self.rect
+
 
 # Główna klasa gry
 class Main:
@@ -104,25 +125,57 @@ class Main:
         i_top = randint(-350, -100)
         i_bot = i_top + GAP + PIPE_HEIGHT
         self.pipes.append((Pipe(WIDTH, i_top, True), Pipe(WIDTH, i_bot)))
+        # tworzymy parę złożoną z górnej i dolnej rury
+    
+    def handle_hits(self, bird_rect):
+        for pipe_pair in self.pipes:
+            pipe_top, pipe_bot = pipe_pair
+            pipe_top_rect = pipe_top.get_rect()
+            pipe_bot_rect = pipe_bot.get_rect()
+            
+            if bird_rect.colliderect(pipe_top_rect):
+                print("Hittop")
+            if bird_rect.colliderect(pipe_bot_rect):
+                print("Hitbot")
+        if bird_rect.colliderect(self.base.get_rect()):
+            print("Base")
+            
+            
+
 
     def display(self):
         self.background.display(self.WIN)
         self.bird.display(self.WIN)
-        	
         for pipe_pair in self.pipes:
             pipe_pair[0].display(self.WIN)
             pipe_pair[1].display(self.WIN)
         self.base.display(self.WIN)
+
+    def handle_pipes(self):
+        pipes_to_remove = []
+
+        for i in range(len(self.pipes)):
+            if self.pipes[i][0].x == WIDTH // 2:
+                self.generate_pipes()
+            elif self.pipes[i][0].x < -65:
+                pipes_to_remove.append(self.pipes[i])
+
+        for pipe in pipes_to_remove:
+            self.pipes.remove(pipe)    
+        
 
     def main(self):
 
         pygame.init()
         pygame.display.set_caption("Flappy Bird")
 
-        run = True
-        
-        while run:
+        #clock = pygame.time.Clock()
 
+        run = True
+
+        while run:
+            bird_rect = self.bird.get_rect()
+            #clock.tick(FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -130,22 +183,19 @@ class Main:
                 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
+                        #bird_rect = pygame.Rect()
                         self.bird.jump()
 
             for pipe_pair in self.pipes:
                 pipe_pair[0].move()
                 pipe_pair[1].move()
-
             
+            self.handle_hits(bird_rect)
 
-            if self.pipes[0][0].x == WIDTH // 2:
 
-                self.generate_pipes()
-
-            if self.pipes[0][0].x < -65:
-                self.pipes.pop(0) 
-                self.generate_pipes()
-
+            # handle pipes
+            
+            self.handle_pipes()
             self.display()
             self.bird.update()
 
