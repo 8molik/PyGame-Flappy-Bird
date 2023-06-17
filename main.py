237548a43cf109da4e0c81934ea4fd820546879e
8molik <blejzoo@gmail.com>
2023-06-17 12:39:16 +0200

    Added restart button

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ FPS = 60
 WHITE = (255, 255, 255)
 HEIGHT = 600
 WIDTH = 400
-GAP = 150
+GAP = 130
 
-PIPE_WIDTH = 65
-PIPE_HEIGHT = 400
-BIRD_WIDTH = 42
-BIRD_HEIGHT = 29
-NUMBER_HEIGHT = 36
-NUMBER_WIDTH = 24
+PIPE_WIDTH, PIPE_HEIGHT = 65, 400
+BIRD_WIDTH, BIRD_HEIGHT = 42, 29
+NUMBER_WIDTH, NUMBER_HEIGHT = 24, 36
+GAMEOV_WIDTH, GAMEOV_HEIGHT = 192, 42
+BUTTON_WIDTH, BUTTON_HEIGHT = 107, 37
+START_WIDTH, START_HEIGHT = WIDTH // 2 + 30, HEIGHT // 2 + 25
 
 HIT = pygame.USEREVENT + 1
 
@@ -36,53 +36,6 @@ BIRD = [pygame.transform.scale(
             pygame.image.load(os.path.join('assets/GameObjects', 'bird-midflip.png')), (BIRD_WIDTH, BIRD_HEIGHT)),
         pygame.transform.scale(
             pygame.image.load(os.path.join('assets/GameObjects', 'bird-upflip.png')), (BIRD_WIDTH, BIRD_HEIGHT))]       
-
-class Menu:
-    def __init__(self, x = WIDTH//4, y = HEIGHT // 4):
-        self.height = HEIGHT
-        self.width = WIDTH
-        self.x = x
-        self.y = y
-        self.start_image = pygame.transform.scale(
-            pygame.image.load(os.path.join('assets/UI', 'message.png')), (WIDTH//2 + 30, HEIGHT//2 + 25))
-        self.end_image = pygame.transform.scale(
-            pygame.image.load(os.path.join('assets/UI', 'gameover.png')), (192, 42))
-
-    def display_start(self, WIN):
-        WIN.blit(self.start_image, (WIDTH // 4 - 15, HEIGHT // 4  - BIRD_HEIGHT * 2 + 2))
-    
-    def display_end(self, WIN):
-        WIN.blit(self.end_image, (WIDTH // 4, HEIGHT // 4))
-
-class Score:
-    def __init__(self, score = 0):
-        self.score = score
-        self.number = NUMBERS['0']
-
-    def update_score(self):
-        self.score += 1
-        self.number = self.generate_number()
-
-    def generate_number(self):
-        num = str(self.score)
-
-        images = []  # Lista obrazów reprezentujących cyfry składające liczbę
-
-        for digit in num:
-            image = NUMBERS[digit].convert_alpha()  # Konwersja na format z alfą
-            image.set_alpha(255)  # Ustawienie maksymalnej wartości przezroczystości
-            images.append(image)
-    
-        # Tworzenie jednego obrazu zawierającego wszystkie cyfry obok siebie
-        number_surface = pygame.Surface((len(num) * NUMBER_WIDTH, NUMBER_HEIGHT), pygame.SRCALPHA)
-        x_offset = 0
-        for image in images:
-            number_surface.blit(image, (x_offset, 0))
-            x_offset += image.get_width()
-        return number_surface
-
-    def display_score(self, WIN):
-        WIN.blit(self.number, (WIDTH // 2-10, 50))
 
 # Klasa abstrakcyjna reprezentująca obiekt w grze
 class GameObject(ABC):
@@ -128,7 +81,6 @@ class Base(GameObject):
 class Bird(GameObject):
     def __init__(self, x, y, bird_width = BIRD_WIDTH, bird_height = BIRD_HEIGHT):
         super().__init__(x, y, bird_width, bird_height)
-
         self.bird_image = BIRD     
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.vel = 0
@@ -138,30 +90,28 @@ class Bird(GameObject):
     def jump(self): 
         if self.y > 0:
             self.vel = -8
-        
-    
-    def update(self, x = False):
+
+    def update(self, falling = False):
         self.y += self.vel
         self.vel += 0.5
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-        #obsługa animacji skoku
+        #Jump anitmation
         if self.vel < -4:
             self.image_index = 2
         elif self.vel < 0:
             self.image_index = 1
         else:
             self.image_index = 0
-
         self.image = self.bird_image[self.image_index]
         self.image = pygame.transform.rotate(self.image, self.vel * -2)
 
-        if x:
+        if falling:
             self.image = pygame.transform.rotate(self.image, -90)
         
         if self.y >= (HEIGHT - 100 - self.height):
             self.vel = 0
 
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
     
     def display(self, WIN):
         WIN.blit(self.image, (self.x, self.y))
@@ -190,35 +140,90 @@ class Pipe(GameObject):
     def get_rect(self):
         return self.rect
 
+class Menu:
+    def __init__(self):
+        self.x =  WIDTH // 4
+        self.y = HEIGHT // 4
+        self.height = HEIGHT
+        self.width = WIDTH
+        self.start = pygame.transform.scale(
+            pygame.image.load(os.path.join('assets/UI', 'message.png')), (START_WIDTH, START_HEIGHT))
+        self.game_over = pygame.transform.scale(
+            pygame.image.load(os.path.join('assets/UI', 'gameover.png')), (GAMEOV_WIDTH, GAMEOV_HEIGHT))
+        self.restart_button = pygame.transform.scale(
+            pygame.image.load(os.path.join('assets/UI', 'restart.png')), (BUTTON_WIDTH, BUTTON_HEIGHT))
+        self.restart_rect = pygame.Rect(WIDTH // 2 - BUTTON_WIDTH // 2, HEIGHT // 2, BUTTON_WIDTH, BUTTON_HEIGHT)
+
+    def display_start(self, WIN):
+        WIN.blit(self.start, (WIDTH // 2 - START_WIDTH // 2, HEIGHT // 4  - BIRD_HEIGHT * 2 + 2))
+    
+    def display_end(self, WIN):
+        WIN.blit(self.game_over, (WIDTH // 2 - GAMEOV_WIDTH // 2, HEIGHT // 4))
+        WIN.blit(self.restart_button, (WIDTH // 2 - BUTTON_WIDTH // 2, HEIGHT // 2))
+        self.restart_rect = pygame.Rect(WIDTH // 2 - BUTTON_WIDTH // 2, HEIGHT // 2, BUTTON_WIDTH, BUTTON_HEIGHT)
+
+class Score:
+    def __init__(self, score = 0):
+        self.score = score
+        self.number = NUMBERS['0']
+
+    def update_score(self):
+        self.score += 1
+        self.number = self.generate_number()
+
+    def generate_number(self):
+        num = str(self.score)
+
+        # List of number pictures
+        images = []  
+
+        for digit in num:
+            image = NUMBERS[digit].convert_alpha() 
+            image.set_alpha(255)
+            images.append(image)
+    
+        # Creating picture of multiple numbers
+        number_surface = pygame.Surface((len(num) * NUMBER_WIDTH, NUMBER_HEIGHT), pygame.SRCALPHA)
+        x_offset = 0
+        for image in images:
+            number_surface.blit(image, (x_offset, 0))
+            x_offset += image.get_width()
+        return number_surface
+
+    def display_score(self, WIN):
+        WIN.blit(self.number, (WIDTH // 2 - 10, 50))
+
 class Main:
     def __init__(self):
-        self.WIN = pygame.display.set_mode((WIDTH, HEIGHT))
         self.background = Background(0, 0)
         self.bird = Bird(WIDTH // 2 - 21, HEIGHT // 2)
         self.base = Base(0, HEIGHT - 100)
+        self.score = Score()
+        self.menu = Menu()
+        self.WIN = pygame.display.set_mode((WIDTH, HEIGHT))
         self.pipes = []
         self.generate_pipes()
-        self.score = Score()
-        self.start_screen = Menu()
         self.game_started = False
         self.game_ended = False
     
     def generate_pipes(self):
         i_top = randint(-350, -100)
         i_bot = i_top + GAP + PIPE_HEIGHT
+        # Pair of top pipe and bottom pipe
         self.pipes.append((Pipe(WIDTH, i_top, True), Pipe(WIDTH, i_bot)))
-        # tworzymy parę złożoną z górnej i dolnej rury
     
     def handle_hits(self, bird_rect):
         for pipe_pair in self.pipes:
             pipe_top, pipe_bot = pipe_pair
             pipe_top_rect = pipe_top.get_rect()
             pipe_bot_rect = pipe_bot.get_rect()
-                
+    
             if bird_rect.colliderect(pipe_top_rect):
                 pygame.event.post(pygame.event.Event(HIT))
+
             if bird_rect.colliderect(pipe_bot_rect):
                 pygame.event.post(pygame.event.Event(HIT))
+                
         if bird_rect.colliderect(self.base.get_rect()):
             pygame.event.post(pygame.event.Event(HIT))
 
@@ -234,10 +239,10 @@ class Main:
         else:
             self.background.display(self.WIN)
             self.base.display(self.WIN)
-            self.start_screen.display_start(self.WIN)
+            self.menu.display_start(self.WIN)
             self.bird.display(self.WIN)
         if self.game_ended:
-            self.start_screen.display_end(self.WIN)
+            self.menu.display_end(self.WIN)
         pygame.display.update()
 
     def handle_pipes(self):
@@ -276,23 +281,41 @@ class Main:
                         if not self.game_started:  # Jeśli gra się nie rozpoczęła, rozpocznij ją po naciśnięciu spacji
                             self.game_started = True
                             self.bird.jump()
-                        else:
+                        elif not self.game_ended:
                             self.bird.jump()
-                        if self.game_ended:
-                            self.__init__()
+                    
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.game_ended:
+                        mouse_pos = pygame.mouse.get_pos()
+                        button_rect = self.menu.restart_rect
+                        print("Piwo")
+                        print(button_rect)
+                        print(mouse_pos)
+                        if button_rect.collidepoint(mouse_pos):
+                            if pygame.mouse.get_pressed()[0] == 1:
+                                # Restart the game
+                                self.__init__()
+                            
+                           
+                
 
                 if event.type == HIT:
                     self.bird.update(True)
                     self.game_ended = True
 
-            if self.game_started  and not self.game_ended:
-                for pipe_pair in self.pipes:
-                    pipe_pair[0].move()
-                    pipe_pair[1].move()
+            if self.game_started:
+                if not self.game_ended:
+                    for pipe_pair in self.pipes:
+                        pipe_pair[0].move()
+                        pipe_pair[1].move()
                 
                 self.handle_hits(bird_rect)
                 self.handle_pipes()
                 self.bird.update()
+
+                if self.bird.y >= (HEIGHT - 100 - self.bird.height):
+                    self.game_ended = True
+                    
             
             self.display()
 
